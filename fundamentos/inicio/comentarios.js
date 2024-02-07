00efa80564355f92695c42d56e67f24c0d3ced7d
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-06 22:43:53 -0300

    comentários no javascript

---
+++ fundamentos/inicio/comentarios.js
@@ -0,0 +1,14 @@
+// console.log é usado para exibir algo na tela do computador!
+
+console.log("Passo #01");
+console.log("Passo #02"); // mais um exemplo de comentário
+// console.log("Passo #03");
+
+/*
+    Esse pe um
+    comentário
+    de múltiplas
+    linhas
+*/
+
+console.log("Passo #03");
