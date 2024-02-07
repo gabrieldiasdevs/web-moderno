34450df17b81cf131e9b889a7d9080f925cfad7a
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-07 12:03:52 -0300

    js

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI * raio * raio
+
+const raio = 10;
+const PI = 3.14;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + PI * raio * raio + "m2.");
+console.log("O valor da área é " + areaCirc + "m2.");
+
+areaCirc = Math.PI * raio * raio;
+console.log("O valor da área é " + areaCirc + "m2.")
---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+
+// trocar o valor da a pelo o da b
+
+let temp = a;
+
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
