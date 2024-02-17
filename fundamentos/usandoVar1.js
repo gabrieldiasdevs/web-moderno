4d790ec85be845b488df5e88235cfc0669daf150
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-17 18:17:59 -0300

    var

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 1233
+}
+
+teste()
+console.log(local)
